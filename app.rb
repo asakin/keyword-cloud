0c5c6a430f7aca5b69fb029bec5a0eee86e77ccf
Ariel Sakin <ariel@skycure.com>
2013-07-29 02:24:30 +0300

    Finished scaffolding sinatra, angular.js, and lineman for Heroku

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
-get '/' do
-  "Hello World!"
+configure :development do
+  set :public_folder, Proc.new { File.join(root, 'front_end/dist') }
 end
 
+get '/api/V1/index.html' do
+  'Hello API'
+end
+
+# Catch all routes being passed to the angular app
+get '/*' do
+  erb :index
+end
+
+post '/*' do
+  erb :index
+end
