10d5b48c8cd6435493ad0f08a6a7711a50416de6
Ariel Sakin <ariel@skycure.com>
2013-07-31 16:54:48 +0300

    Add json_response to API

--- app.rb
+++ app.rb
@@ -1,15 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 configure :development do
   set :public_folder, Proc.new { File.join(root, 'front_end/dist') }
 end
 
-get '/api/V1/index.html' do
-  'Hello API'
+get '/api/V1/index', :provides => :json do
+  content_type :json
+  json_status 404, "Not fosund"
 end
 
 
+
+# A utility method for setting get_or_post routes
 def get_or_post(path, opts={}, &block)
   get(path, opts, &block)
   post(path, opts, &block)
@@ -18,4 +22,11 @@ end
 # Catch all routes being passed to the angular app
 get_or_post '/*' do
   send_file File.join(settings.public_folder, 'index.html')
+end
+
+helpers do
+  def json_status(code, message)
+    status(code)
+    { :status => code, :message => message }.to_json
+  end
 end
